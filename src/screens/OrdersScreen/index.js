64076825a8b1f4839ee689935f59a3c88c69b3cb
AmirthaShankari <amirthashankari@gmail.com>
2020-10-15 19:22:39 +0530

    swipable order cards implementation

--- src/screens/OrdersScreen/index.js
+++ src/screens/OrdersScreen/index.js
@@ -1,31 +1,137 @@
-import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  View, Text, ActivityIndicator, ScrollView
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 // App Imports
 import ReactNativeSwipeableViewStack from 'react-native-swipeable-view-stack';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { Context as AuthContext } from '../../context/AuthContext';
 import { Header } from '../../components';
 import { AppMessages } from '../../constants/AppMessages';
 import { styles } from './styles';
-import { Colors } from '../../themes';
-// const ReactNativeSwipeableViewStack = require('react-native-swipeable-view-stack');
+import { Colors, Metrics } from '../../themes';
 
 const APP_MSG = AppMessages.SCREENS.ORDERS;
-// ReactNativeSwipeableViewStack();
 
 const dataArray = [1, 2];
 
-const OrdersScreen = ({ navigation }) => {
+const OrdersScreen = () => {
+  const [ordersDetail, setOrdersDetail] = useState();
   const { state: { authName } } = useContext(AuthContext);
 
-  const renderViewItem = (item) => (
-    <View style={{
-      width: 100, height: 200, backgroundColor: Colors.blue_grey
-    }}
-    >
-      <Text>{item}</Text>
+  // Use effect declarations
+  useEffect(() => {
+    const data = {
+      totalOrders: 134,
+      activeDays: 634,
+      rating: '78%',
+      orders: [{
+        key: '13174',
+        status: 'Delivered',
+        orderNumber: 13174,
+        totals: '$349,99',
+        items: [
+          {
+            id: '1',
+            item: 'Parrot Summe T-shirt XXS'
+          },
+          {
+            id: '2',
+            item: 'Sunglasses'
+          },
+        ],
+        delivery: {
+          name: 'Ola Levinsky',
+          addr1: '6 Sierpnia 1/20A',
+          addr2: 'Warsaw, Poland'
+        }
+      },
+      {
+        key: '13175',
+        status: 'Delivered',
+        orderNumber: 13175,
+        totals: '$349,99',
+        items: [
+          {
+            id: '1',
+            item: 'Parrot Summe T-shirt XXS'
+          },
+          {
+            id: '2',
+            item: 'Sunglasses'
+          },
+        ],
+        delivery: {
+          name: 'Ola Levinsky',
+          addr1: '6 Sierpnia 1/20A',
+          addr2: 'Warsaw, Poland'
+        }
+      }
+      ]
+    };
+    setOrdersDetail(data);
+  }, []);
+
+  const renderViewItem = (order) => (!order ? <View style={{ width: 100, height: 100 }} /> : (
+    <View style={styles.orderCard}>
+      <View style={styles.orderCardHeader}>
+        <View>
+          <Text style={styles.cardHeaderLabel}>
+            {APP_MSG.ORDER_NUMBER}
+          </Text>
+          <Text style={styles.orderNumber}>
+            {order.orderNumber}
+          </Text>
+        </View>
+        <View style={styles.statusWrapper}>
+          <Text style={styles.cardHeaderLabel}>
+            {APP_MSG.STATUS}
+          </Text>
+          <Text style={styles.orderStatus}>
+            {order.status}
+          </Text>
+        </View>
+        <View style={styles.logo} />
+      </View>
+      <View style={styles.cardSectionSeparator} />
+      <View style={styles.itemsSummaryWrapper}>
+        <View>
+          <Text style={styles.cardHeading}>{APP_MSG.ITEMS}</Text>
+          <FlatList
+            data={order.items}
+            keyExtractor={((item) => item.id)}
+            renderItem={({ item }) => renderOrderItems(item)}
+          />
+        </View>
+        <View>
+          <Text style={styles.cardHeading}>{APP_MSG.SUMMARY}</Text>
+          <Text style={styles.totals}>{order.totals}</Text>
+        </View>
+      </View>
+      <View style={styles.cardSectionSeparator} />
+      <View>
+        <View style={styles.addrAndTracking}>
+          <View>
+            <Text style={styles.cardHeading}>{APP_MSG.DELIVERY}</Text>
+            <Text style={styles.items}>{ordersDetail.orders[0].delivery.name}</Text>
+            <Text style={styles.items}>{ordersDetail.orders[0].delivery.addr1}</Text>
+            <Text style={styles.items}>{ordersDetail.orders[0].delivery.addr2}</Text>
+          </View>
+          <View>
+            <View>
+              <Text style={styles.cardHeading}>{APP_MSG.TRACKING}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.cardSectionSeparator} />
     </View>
+  ));
+
+  const renderOrderItems = (item) => (
+    <Text style={styles.items}>{item.item}</Text>
   );
 
   return (
@@ -37,38 +143,101 @@ const OrdersScreen = ({ navigation }) => {
         showLogo
         showProfile
       />
-      <View style={styles.ordersScreenWrapper}>
-        <View style={styles.userDetail}>
-          <Text style={styles.userName}>{ authName }</Text>
-          <Icon
-            name="ellipsis-horizontal-outline"
-            size={30}
-            color={Colors.blue_grey}
-          />
-        </View>
-        <View style={styles.orderInsightWrapper}>
-          <View>
-            <Text>134</Text>
-            <Text>{APP_MSG.ORDERS}</Text>
+      {(ordersDetail) ? (
+        <View style={styles.ordersScreenWrapper}>
+          <View style={styles.userDetail}>
+            <Text style={styles.userName}>{ authName }</Text>
+            <Icon
+              name="ellipsis-horizontal-outline"
+              size={30}
+              color={Colors.blue_grey}
+            />
           </View>
-          <View>
-            <Text>634</Text>
-            <Text>{APP_MSG.DAYS_ACTIVE}</Text>
+          <View style={styles.orderInsightWrapper}>
+            <View style={[styles.ordersInsight, styles.ordersCountWrapper]}>
+              <Text style={styles.orderInsightVal}>{ordersDetail.totalOrders}</Text>
+              <Text style={styles.orderInsightLabel}>{APP_MSG.ORDERS}</Text>
+            </View>
+            <View style={[styles.ordersInsight, styles.activeDaysWrapper]}>
+              <Text style={styles.orderInsightVal}>{ordersDetail.activeDays}</Text>
+              <Text style={styles.orderInsightLabel}>{APP_MSG.DAYS_ACTIVE}</Text>
+            </View>
+            <View style={[styles.ordersInsight, styles.ratingWrapper]}>
+              <Text style={styles.orderInsightVal}>{ordersDetail.rating}</Text>
+              <Text style={styles.orderInsightLabel}>{APP_MSG.ORDERS_RATING}</Text>
+            </View>
           </View>
-          <View>
-            <Text>78%</Text>
-            <Text>{APP_MSG.ORDERS_RATING}</Text>
+          <View style={{
+            position: 'relative', flex: 1
+          }}
+          >
+            <View style={{ position: 'absolute', bottom: -40, width: '100%' }}>
+              <ReactNativeSwipeableViewStack
+                initialSelectedIndex={0}
+                data={ordersDetail.orders}
+                renderItem={(element) => renderViewItem(element)}
+                stackSpacing={40}
+              />
+            </View>
           </View>
-        </View>
-        <ReactNativeSwipeableViewStack
-          initialSelectedIndex={1}
-          data={dataArray}
-          renderItem={(element) => renderViewItem(element)}
-          stackSpacing={20}
-          useNativeDriver
-        />
-      </View>
 
+          {/* <View style={styles.orderCard}>
+            <View style={styles.orderCardHeader}>
+              <View>
+                <Text style={styles.cardHeaderLabel}>
+                  {APP_MSG.ORDER_NUMBER}
+                </Text>
+                <Text style={styles.orderNumber}>
+                  {ordersDetail.orders[0].orderNumber}
+                </Text>
+              </View>
+              <View style={styles.statusWrapper}>
+                <Text style={styles.cardHeaderLabel}>
+                  {APP_MSG.STATUS}
+                </Text>
+                <Text style={styles.orderStatus}>
+                  {ordersDetail.orders[0].status}
+                </Text>
+              </View>
+              <View style={styles.logo} />
+            </View>
+            <View style={styles.cardSectionSeparator} />
+            <View style={styles.itemsSummaryWrapper}>
+              <View>
+                <Text style={styles.cardHeading}>{APP_MSG.ITEMS}</Text>
+                <FlatList
+                  data={ordersDetail.orders[0].items}
+                  keyExtractor={((item) => item.id)}
+                  renderItem={({ item }) => renderOrderItems(item)}
+                />
+              </View>
+              <View>
+                <Text style={styles.cardHeading}>{APP_MSG.SUMMARY}</Text>
+                <Text style={styles.totals}>{ordersDetail.orders[0].totals}</Text>
+              </View>
+            </View>
+            <View style={styles.cardSectionSeparator} />
+            <View>
+              <View style={styles.addrAndTracking}>
+                <View>
+                  <Text style={styles.cardHeading}>{APP_MSG.DELIVERY}</Text>
+                  <Text style={styles.items}>{ordersDetail.orders[0].delivery.name}</Text>
+                  <Text style={styles.items}>{ordersDetail.orders[0].delivery.addr1}</Text>
+                  <Text style={styles.items}>{ordersDetail.orders[0].delivery.addr2}</Text>
+                </View>
+                <View>
+                  <View>
+                    <Text style={styles.cardHeading}>{APP_MSG.TRACKING}</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={styles.cardSectionSeparator} />
+          </View> */}
+        </View>
+      ) : (
+        <ActivityIndicator style={styles.inlineLoader} />
+      )}
     </View>
   );
 };
--- src/screens/OrdersScreen/styles/index.js
+++ src/screens/OrdersScreen/styles/index.js
@@ -6,6 +6,10 @@ export const styles = StyleSheet.create({
   ...CommonStyles.screen,
   ordersScreenWrapper: {
     flex: 1,
+    display: 'flex',
+  },
+  inlineLoader: {
+    ...CommonStyles.loader.inlineLoader
   },
   userDetail: {
     display: 'flex',
@@ -25,6 +29,112 @@ export const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    borderColor: Colors.blue_grey,
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    paddingHorizontal: Metrics.padding_2
+  },
+  ordersInsight: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingHorizontal: Metrics.padding_1,
+    paddingVertical: Metrics.padding_1 * 1.5
+  },
+  ordersCountWrapper: {
+    flex: 1,
+  },
+  activeDaysWrapper: {
+    borderRightWidth: 0.5,
+    borderLeftWidth: 0.5,
+    paddingHorizontal: Metrics.padding_2,
+    borderColor: Colors.blue_grey
+  },
+  ratingWrapper: {
+    flex: 1,
+  },
+  orderInsightVal: {
+    ...CommonStyles.fontWeights.w500,
+    fontSize: 22,
+    color: Colors.black
+  },
+  orderInsightLabel: {
+    ...CommonStyles.fontWeights.w400,
+    fontSize: 14,
+    color: Colors.black
+  },
+  orderCard: {
+    width: Metrics.screenWidth - (Metrics.padding_2 * 2),
+    height: Metrics.screenHeight * 0.5,
+    backgroundColor: Colors.blue_grey,
+    paddingVertical: Metrics.padding_2,
+    margin: Metrics.margin_2,
+    borderRadius: 10,
+    overflow: 'hidden'
+  },
+  orderCardHeader: {
+    paddingHorizontal: Metrics.padding_2,
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between'
+  },
+  cardHeaderLabel: {
+    ...CommonStyles.fontWeights.w400,
+    fontSize: 14,
+    color: Colors.white
+  },
+  orderNumber: {
+    ...CommonStyles.fontWeights.w500,
+    fontSize: 18,
+    color: Colors.white
+  },
+  statusWrapper: {
+    flex: 1,
+    display: 'flex',
+  },
+  orderStatus: {
+    ...CommonStyles.fontWeights.w500,
+    fontSize: 18,
+    color: Colors.green
+  },
+  logo: {
+    flex: 1
+  },
+  cardSectionSeparator: {
+    borderTopWidth: 0.5,
+    borderColor: Colors.white,
+    marginVertical: Metrics.margin_2
+  },
+  items: {
+    ...CommonStyles.fontWeights.w400,
+    fontSize: 16,
+    color: Colors.white,
+    paddingBottom: Metrics.padding_1 * 0.5
+  },
+  cardHeading: {
+    ...CommonStyles.fontWeights.w500,
+    fontSize: 18,
+    color: Colors.white,
+    paddingBottom: Metrics.padding_1
+  },
+  itemsSummaryWrapper: {
+    paddingHorizontal: Metrics.padding_2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+  },
+  totals: {
+    ...CommonStyles.fontWeights.w700,
+    fontSize: 24,
+    color: Colors.white,
+  },
+  addrAndTracking: {
+    paddingHorizontal: Metrics.padding_2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
   }
 });
