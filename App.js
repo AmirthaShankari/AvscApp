7c9d8e63ecef3252d9e0a55d796cc15e7fefde4a
AmirthaShankari <amirthashankari@gmail.com>
2020-10-29 19:24:07 +0530

    Added splash screen

--- App.js
+++ App.js
@@ -112,14 +112,20 @@
 // });
 
 // export default App;
-import React from 'react';
+import React, { useEffect } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
+
+// App imports
 import { AppContainer } from './src/routes';
 import { Provider as AppLoadProvider } from './src/context/AppLoadContext';
 import { Provider as AuthProvider } from './src/context/AuthContext';
 
 export default () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   Ionicons.loadFont();
   return (
     <AppLoadProvider>
